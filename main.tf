dc364ed27ff7e3bad070d5e3f1b2cad0f996a45c
Daniel Adedokun <adedokundaniel16@gmail.com>
2025-07-13 04:07:27 +0100

    Add Terraform file

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "daniel-project-462315"
+  credentials = file("daniel-project-462315-4ab57b348593.json")
+}
+
+resource "google_compute_instance" "test-vm" {
+  name         = "my-vm"
+  machine_type = "e2-micro"          
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    subnetwork = "default"
+
+    access_config {  
+    }
+  }
+}
